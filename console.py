4277ab6f6e331f9bb3040f5f491f324e75b14a02
Hovixen <orevaerijo@gmail.com>
2023-12-05 14:00:15 +0000

    updated the console

--- console.py
+++ console.py
@@ -1,2 +1,15 @@
 #!/usr/bin/python3
+
 import cmd
+""" This console is for manipulating objects with certain comands """
+
+
+class HBNBCommand(cmd.Cmd):
+    """ The class responsible for the commands to manipulate the objects """
+    prompt = "(hbnb) "
+
+    do_
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
