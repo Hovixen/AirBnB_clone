d2e9dadb63621f06590f6b94016e5d5caeb152cb
Hovixen <orevaerijo@gmail.com>
2023-12-06 14:08:10 +0000

    updated the base_model

--- models/base_model.py
+++ models/base_model.py
@@ -4,7 +4,7 @@
 
 import uuid
 from datetime import datetime
-import models
+
 
 
 class BaseModel():
@@ -23,14 +23,11 @@ class BaseModel():
     def save(self):
         """ save method """
         self.updated_at = datetime.now()
-        models.storage.save()
+ 
 
     def to_dict(self):
         """ method returns a dict with keys/values """
-        new_dict = {}
-        for key in dir(self):
-            if not key.startswith("__") and not collable(getattr(self, key)):
-                new_dict[key] = getattr(self, key)
+        new_dict = self.__dict__.copy()
         new_dict["__class__"] = self.__class__.__name__
         new_dict["created_at"] = self.created_at.isoformat()
         new_dict["updated_at"] = self.updated_at.isoformat()
