ac271b0cd6749b97aac51e757a87f242aa35a54d
Hovixen <orevaerijo@gmail.com>
2023-12-08 13:49:20 +0000

    do_create method updated

--- console.py
+++ console.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
 
 import cmd
+from models.base_model import BaseModel
+from models import storage
 """ This console is for manipulating objects with certain comands """
 
 
 class HBNBCommand(cmd.Cmd):
     """ The class responsible for the commands to manipulate the objects """
     prompt = "(hbnb) "
+
+    list_cls = [BaseModel]
+    class_dic = {classes.__name__: classes for classes in list_cls}
 
     def do_quit(self, line):
         """ Quit command to exit the program """
@@ -21,6 +26,20 @@ class HBNBCommand(cmd.Cmd):
         """ does not execute anything for an empty line """
         pass
 
+    def do_create(self, arg):
+        """
+        Creates a new instance of a specified class, saves it to the JASON file
+        and prints its ID
+        Syntax: create <class_name>
+        """
+        if not arg:
+            print("** class name missing **")
+        elif arg not in [cls.__name__ for cls in HBNBCommand.list_cls]:
+            print("** class doesn't exist **")
+        else:
+            instance_model = HBNBCommand.class_dic[arg]()
+            instance_model.save()
+            print(instance_model.id)
 
 if __name__ == '__main__':
     # try:
