7f4268f728c443bd2cba4012f437a64dc46e5a11
Hovixen <orevaerijo@gmail.com>
2023-12-08 08:28:06 +0000

    fix the import line

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,7 +3,8 @@
 
 
 import json
-# from models.base_model import BaseModel
+import os
+from models.base_model import BaseModel
 
 class FileStorage():
     """ class FileStorage"""
@@ -24,17 +25,21 @@ class FileStorage():
         dic = {}
         for key, obj in self.__objects.items():
             dic[key] = obj.to_dict()
-        with open(self.__file_path, mode = "w", encoding = "utf-8") as file:
+        with open(self.__file_path, 'w', encoding="utf-8") as file:
             json.dump(dic, file)
 
     def reload(self):
-        """ loads objects from file """
-        try:
-            with open(self.__file_path, mode = "r", encoding = "utf-8") as file:
+        """ loads objects from existing file.jason """
+        # a class mapping dictionary to get the class
+
+        cls_dic = {'BaseModel': BaseModel}
+
+        j_file = os.path.exits(self.__file_path)
+        if j_file:
+            with open(self.__file_path, 'r', encoding="utf-8") as file:
                 new = json.load(file)
-                for key, obj_dict in new.items():
-                    class_name, obj_id = key.split('.')
-                    obj = globals()[class_name](**obj_dict)
-                    self.__objects[key] = obj
-        except FileNotFoundError:
-            pass
+                for key, obj_dic in new.items():
+                    get_class = obj_dic.get(__class__)
+                    obj_class = cls_dic[get_class]
+                    obj = obj_class(**value)
+                    self.new(obj)
